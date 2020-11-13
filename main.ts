fd4b6cfe6fcaab62f6034c1d6c077d2dcbdf61a0
NatashaG6 <74411619+NatashaG6@users.noreply.github.com>
2020-11-13 12:39:17 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Diamond)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.forever(function () {
+	
+})
